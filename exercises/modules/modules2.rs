277ad7b04d5490d11b3ef4d4444eb56d1391201c
Jingren Zhou <jingrenzhou302@gmail.com>
2024-07-11 08:32:29 +0800

    update
    Signed-off-by: Jingren Zhou <jingrenzhou302@gmail.com>

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,12 +7,12 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    //pub use self::fruits::PEAR as fruit;
+    pub use self::fruits::PEAR as veggie;
+//    pub use self::veggies::CUCUMBER as veggie;
+    pub use self::veggies::CUCUMBER as fruit;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
